21cf5b449e5f02e3e36eccc9fd6165939697f715
Pendukung-OkeGas <muhammadfaizun.23178@mhs.unesa.ac.id>
2026-06-03 19:31:09 +0700

    delete unused page

--- app/sign-up/page.tsx
+++
@@ -1,9 +0,0 @@
-import { getUser } from '@/lib/session'
-import { redirect } from 'next/navigation'
-import { AuthForm } from '@/components/auth-form'
-
-export default async function SignUpPage() {
-  const session = await getUser()
-  if (session?.id) redirect('/')
-  return <AuthForm mode="sign-up" />
-}
